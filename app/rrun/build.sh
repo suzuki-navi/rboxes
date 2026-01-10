6d837b3156771bbe25234a092996a92994e1e0ee
suzuki-navi <suzuki.navi.dev@gmail.com>
2026-01-10 18:52:52 +0900

    feat(rrun): add reproducible run environment tool

---
+++ app/rrun/build.sh
@@ -0,0 +1,29 @@
+
+set -e
+
+script_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+app_name="$(basename "$script_dir")"
+
+cd $script_dir
+
+source ../../lib/lib.sh
+
+mkdir -p ./var/etc
+
+build_app ../rselfpack
+smart_cp ../rselfpack/rselfpack ./var/etc/rselfpack
+
+mkdir -p ./var/src
+
+smart_cp ./main.sh ./var/src/main.sh
+
+build_app ../extractmarkdown
+smart_cp ../extractmarkdown/extractmarkdown ./var/src/extractmarkdown
+
+build_app ../rdockrun
+smart_cp ../rdockrun/rdockrun ./var/src/rdockrun
+
+./var/etc/rselfpack -x --compress ./var/src -o ./$app_name.tmp
+smart_mv ./$app_name.tmp ./$app_name
+chmod +x ./$app_name
+add_gitignore /$app_name
---
+++ app/rrun/main.sh
@@ -0,0 +1,48 @@
+
+set -e
+
+script_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+pwd=$(pwd)
+
+CONFIG_MD_FILE_NAME="rrun.md"
+
+# Find the config markdown file in current or parent directories
+config_md_file_path=""
+current_dir="$pwd"
+while [ "$current_dir" != "/" ]; do
+    if [ -f "$current_dir/$CONFIG_MD_FILE_NAME" ]; then
+        config_md_file_path="$current_dir/$CONFIG_MD_FILE_NAME"
+        break
+    fi
+    current_dir=$(dirname "$current_dir")
+done
+
+if [ -z "$config_md_file_path" ]; then
+    echo "Error: $CONFIG_MD_FILE_NAME not found in current or parent directories." >&2
+    exit 1
+fi
+
+mount_home_dir_path=$(dirname "$config_md_file_path")/.rrun.home
+mkdir -p "$mount_home_dir_path"
+
+mkdir -p "$HOME/.rrun/cache"
+tmpctx="$(mktemp -d $HOME/.rrun/cache/XXXXXX)"
+cleanup() { rm -rf "$tmpctx"; }
+trap cleanup EXIT
+
+$script_dir/extractmarkdown -d "$tmpctx" "$config_md_file_path"
+
+RDOCKRUN_OPTIONS="--pwd -v $tmpctx:$tmpctx -v $mount_home_dir_path:$HOME"
+
+if [ -f "$tmpctx/RDOCKRUN_ENV" ]; then
+    RDOCKRUN_OPTIONS="$RDOCKRUN_OPTIONS --envfile $tmpctx/RDOCKRUN_ENV"
+
+    additional_options=$(
+        RDOCKRUN_OPTIONS=""
+        . "$tmpctx/RDOCKRUN_ENV"
+        echo "$RDOCKRUN_OPTIONS"
+    )
+    RDOCKRUN_OPTIONS="$RDOCKRUN_OPTIONS $additional_options"
+fi
+
+$script_dir/rdockrun $RDOCKRUN_OPTIONS "$tmpctx" "$@"
